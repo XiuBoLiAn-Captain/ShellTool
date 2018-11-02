a7f75b801fb5d3417d57cbf1c975f1e4f38b7054
octuplesakura <2211503711@qq.com>
2018-11-02 11:00:59 +0800

    feat:添加变更脚本

---
+++ stash.js
@@ -0,0 +1,15 @@
+
+require('shelljs/global')
+const argv = require('yargs').argv
+
+//目标分支
+const aims_branch = argv._
+
+echo('存储变更...')
+exec('git stash')
+
+echo('切换分支到' + aims_branch)
+exec('git checkout ' + aims_branch)
+
+echo('变更完成QAQ')
+exec('git stash pop')
