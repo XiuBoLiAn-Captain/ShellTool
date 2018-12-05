174aa69a62a96dd0b049cf667412f401f48ad310
octuplesakura <octuplesakura@gmail.com>
2018-12-05 10:24:37 +0800

    refact:quick_rebase 添加变更判断

--- bin/quick_rebase.js
+++ bin/quick_rebase.js
@@ -8,15 +8,21 @@ const current_branch = exec('git rev-parse --abbrev-ref HEAD').split(',')[0]
 
 const aims_branch = argv._
 
-exec('git checkout ' + aims_branch)
-echo('切换到'+ aims_branch +'分支')
+if(!exec('git status --porcelain')) {
+  
+  exec('git checkout ' + aims_branch)
+  echo('切换到'+ aims_branch +'分支')
+  
+  exec('git pull origin ' + aims_branch)
+  echo('获取更新')
+  
+  exec('git checkout ' + current_branch)
+  echo('切换回'+ current_branch +'分支')
+  
+  echo('正在变基=_=')
+  exec('git rebase ' + aims_branch)
+  echo('你变基啦!QAQ')
 
-exec('git pull origin ' + aims_branch)
-echo('获取更新')
-
-exec('git checkout ' + current_branch)
-echo('切换回'+ current_branch +'分支')
-
-echo('正在变基=_=')
-exec('git rebase ' + aims_branch)
-echo('你变基啦!QAQ')
+}else {
+  echo('有未提交的变动哦0.0')
+}
